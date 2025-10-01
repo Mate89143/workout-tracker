907e5d676196547aad30077842f63cd03cceeab2
Mateo <usugaalvarezmateo@gmail.com>
2025-10-01 17:30:13 -0500

    implementación de rutas GET (lista e individual)

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,28 @@
+const usersController = {
+  getAllUsers: (req, res) => {
+    // TODO: Implement
+    res.status(200).json({ message: 'Get all users - Not implemented' });
+  },
+
+  getUserById: (req, res) => {
+    // TODO: Implement
+    res.status(200).json({ message: 'Get user by ID - Not implemented' });
+  },
+
+  createUser: (req, res) => {
+    // TODO: Implement
+    res.status(201).json({ message: 'Create user - Not implemented' });
+  },
+
+  updateUser: (req, res) => {
+    // TODO: Implement
+    res.status(200).json({ message: 'Update user - Not implemented' });
+  },
+
+  deleteUser: (req, res) => {
+    // TODO: Implement
+    res.status(200).json({ message: 'Delete user - Not implemented' });
+  }
+};
+
+module.exports = usersController;
